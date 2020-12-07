9b3e28c5032cbc5491f3c231649e2af220d31b5c
aditya278 <aditya.shukla278@gmail.com>
2020-12-07 17:34:09 +0530

    Added the Pokemon Profile Card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import bgImage from './images/pattern.png';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import bgImage from "./images/pattern.png";
 
-import Navbar from './components/layout/Navbar';
-import Dashboard from './components/layout/Dashboard';
-import Pokemon from './components/pokemon/Pokemon';
+import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/layout/Dashboard";
+import Pokemon from "./components/pokemon/Pokemon";
 
 function App() {
-
   const [loading, setLoading] = useState(true);
   const [pokemons, setPokemons] = useState(null);
   const [pokemonDetails, setPokemonDetails] = useState(null);
@@ -19,7 +18,9 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       setLoading(true);
-      const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const res = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      );
       setPokemons(res.data.results);
       setLoading(false);
     }
@@ -27,100 +28,151 @@ function App() {
   }, []);
 
   const getPokemonDetails = async (pokemonIndex) => {
+    //Getting the Pokemons Details
     const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
-    const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
-    
+
     setLoading(true);
 
     const pokemonRes = await axios.get(pokemonUrl);
-    let [hp, attack, defense, speed, specialAttack, specialDefense] = '';
+    let [hp, attack, defence, speed, specialAttack, specialDefense] = "";
 
     pokemonRes.data.stats.map((stat) => {
-      switch(stat.stat.name) {
-        case 'hp':
-          hp = stat['base_stat'];
+      switch (stat.stat.name) {
+        case "hp":
+          hp = stat["base_stat"];
           break;
-        case 'attack':
-          attack = stat['base_stat'];
+        case "attack":
+          attack = stat["base_stat"];
           break;
-        case 'defense':
-          defense = stat['base_stat'];
+        case "defense":
+          defence = stat["base_stat"];
           break;
-        case 'speed':
-          speed = stat['base_stat'];
+        case "speed":
+          speed = stat["base_stat"];
           break;
-        case 'special-attack':
-          specialAttack = stat['base_stat'];
+        case "special-attack":
+          specialAttack = stat["base_stat"];
           break;
-        case 'special-defense':
-          specialDefense = stat['base_stat'];
+        case "special-defense":
+          specialDefense = stat["base_stat"];
           break;
         default:
           return;
       }
-    })
+    });
 
     //convert decimeter to feet... the +0.0001 * 100) / 100 is for rounding to 2 decimal places
-    const height = Math.round((pokemonRes.data.height * 0.32804 + 0.0001) * 100) / 100;
+    const height =
+      Math.round((pokemonRes.data.height * 0.32804 + 0.0001) * 100) / 100;
 
     //Convert hectograms to kgs
-    const weight = Math.round((pokemonRes.data.height * 0.1 + 0.0001) * 100) / 100;
+    const weight =
+      Math.round((pokemonRes.data.height * 0.1 + 0.0001) * 100) / 100;
 
     const types = pokemonRes.data.types.map((type) => type.type.name);
 
     const abilities = pokemonRes.data.abilities.map((ability) => {
       return ability.ability.name
         .toLowerCase()
-        .split('-')
-        .map(s => s.charAt(0).toUpperCase() + s.substring(1))
-        .join(' ');
-    })
+        .split("-")
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(" ");
+    });
 
-    const evs = pokemonRes.data.stats.filter((stat) => {
-      if(stat.effort > 0) return true;
-      return false;
-    }).map(stat => {
-      return `${stat.effor} ${stat.stat.name}`.toLowerCase()
-      .split('-')
-      .map(s => s.charAt(0).toUpperCase() + s.substring(1))
-      .join(' ');
-    }).join(', ')
+    const evs = pokemonRes.data.stats
+      .filter((stat) => {
+        if (stat.effort > 0) return true;
+        return false;
+      })
+      .map((stat) => {
+        return `${stat.effort} ${stat.stat.name.toLowerCase()
+          .split("-")
+          .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(" ")}`;
+      })
+      .join(", ");
+
+    //Getting the Pokemon Species Details like:
+    //Descriptions, Catch Rate, EggGroups, Gender Ratio, Hatch Steps
+    const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
+    const pokemonSpeciesRes = await axios.get(pokemonSpeciesUrl);
+
+    let description = "";
+    pokemonSpeciesRes.data.flavor_text_entries.some((flavor) => {
+      if (flavor.language.name === "en") {
+        description = flavor.flavor_text;
+        return;
+      }
+    });
+
+    const femaleRate = pokemonSpeciesRes.data["gender_rate"];
+    const genderRatioFemale = 12.5 * femaleRate;
+    const genderRatioMale = 12.5 * (8 - femaleRate);
+
+    const catchRate = Math.round(
+      (100 / 255) * pokemonSpeciesRes.data["capture_rate"]
+    );
+    const eggGroups = pokemonSpeciesRes.data["egg_groups"].map((group) => {
+      return group.name
+        .toLowerCase()
+        .split(" ")
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(" ");
+    });
+
+    const hatchSteps = 255 * (pokemonSpeciesRes.data["hatch_counter"] + 1);
 
     const pokemon = {
-      name : pokemonRes.data.name,
-      imageUrl : pokemonRes.data.sprites.front_default,
-      types : types,
-      description : '',
-      stats : {
-        hp : hp,
+      name: pokemonRes.data.name,
+      imageUrl: pokemonRes.data.sprites.front_default,
+      types: types,
+      description: description,
+      stats: {
+        hp: hp,
         attack: attack,
-        defense: defense,
+        defence: defence,
         speed: speed,
         specialAttack: specialAttack,
-        specialDefense: specialDefense
+        specialDefense: specialDefense,
       },
-      height : height,
-      weight : weight,
-      eggGroup: '',
+      height: height,
+      weight: weight,
+      eggGroups: eggGroups,
       abilities: abilities,
-      genderRatioMale: '',
-      genderRatioFemale: '',
+      genderRatioMale: genderRatioMale,
+      genderRatioFemale: genderRatioFemale,
       evs: evs,
-      hatchSteps: ''
-    }
+      hatchSteps: hatchSteps,
+      catchRate: catchRate,
+    };
 
     setPokemonDetails(pokemon);
     setLoading(false);
-  }
+  };
 
   return (
     <Router>
-      <div className="topDiv" style={{background : `url(${bgImage})`}}>
+      <div className="topDiv" style={{ background: `url(${bgImage})` }}>
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path="/" render={() => (<Dashboard pokemons = {pokemons} loading = {loading} />)} />
-            <Route exact path="/pokemon/:pokemonIndex" render={(params) => (<Pokemon {...params} getPokemonDetails={getPokemonDetails} loading={loading} pokemonDetails={pokemonDetails} />)} />
+            <Route
+              exact
+              path="/"
+              render={() => <Dashboard pokemons={pokemons} loading={loading} />}
+            />
+            <Route
+              exact
+              path="/pokemon/:pokemonIndex"
+              render={(params) => (
+                <Pokemon
+                  {...params}
+                  getPokemonDetails={getPokemonDetails}
+                  loading={loading}
+                  pokemonDetails={pokemonDetails}
+                />
+              )}
+            />
           </Switch>
         </div>
       </div>
--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -1,23 +1,355 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
-import Loading from '../layout/Loading';
+import Loading from "../layout/Loading";
+import pokeballSpinner from "../../images/pokeball.gif";
 
-export default function Pokemon({match, getPokemonDetails, loading}) {
-    
+const TYPE_COLORS = {
+    bug: "B1C12E",
+    dark: "4F3A2D",
+    dragon: "755EDF",
+    electric: "FCBC17",
+    fairy: "F4B1F4",
+    fighting: "823551D",
+    fire: "E73B0C",
+    flying: "A3B3F7",
+    ghost: "6060B2",
+    grass: "74C236",
+    ground: "D3B357",
+    ice: "A3E7FD",
+    normal: "C8C4BC",
+    poison: "934594",
+    psychic: "ED4882",
+    rock: "B9A156",
+    steel: "B5B5C3",
+    water: "3295F6",
+};
+
+export default function Pokemon({
+    match,
+    getPokemonDetails,
+    loading,
+    pokemonDetails,
+}) {
     const pokemonIndex = match.params.pokemonIndex;
+    const [imgLoading, setImgLoading] = useState(true);
 
     useEffect(() => {
         getPokemonDetails(pokemonIndex);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    if(loading) {
-        return <Loading />
+    const updateLoading = (value) => {
+        setImgLoading(value);
+    };
+
+    if (loading) {
+        return <Loading />;
     }
 
+    if (!pokemonDetails) return <Loading />;
+
     return (
-        <div>
-            {pokemonIndex}
+        <div className="col">
+            <Link to="/">
+                <button className="btn btn-primary mb-4">Go to Home</button>
+            </Link>
+            <div className="card">
+                <div className="card-header">
+                    <div className="row">
+                        <div className="col-5">
+                            <h5>{pokemonIndex}</h5>
+                        </div>
+                        <div className="col-7">
+                            <div className="float-right">
+                                {
+                                    //console.log(pokemonDetails.types)
+                                    pokemonDetails.types.map((type) => (
+                                        <span
+                                            key={type}
+                                            className="badge badge-primary badge-pill mr-1"
+                                            style={{
+                                                backgroundColor: `#${TYPE_COLORS[type]}`,
+                                                color: "white",
+                                            }}
+                                        >
+                                            {type
+                                                .toLowerCase()
+                                                .split(" ")
+                                                .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+                                                .join(" ")}
+                                        </span>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-body">
+                    <div className="row align-items-center">
+                        <div className="col-md-3">
+                            {imgLoading ? (
+                                <img
+                                    src={pokeballSpinner}
+                                    style={{ width: "5em", height: "5em" }}
+                                    className="card-img-top rounded mx-auto mt-2"
+                                    alt="loading"
+                                />
+                            ) : null}
+                            <img
+                                src={pokemonDetails.imageUrl}
+                                className="card-img-top rounded mx-auto mt-2"
+                                onLoad={() => updateLoading(false)}
+                                alt="pokemonImg"
+                            />
+                        </div>
+                        <div className="col-md-9">
+                            <h4 className="mx-auto">
+                                {pokemonDetails.name
+                                    .toLowerCase()
+                                    .split(" ")
+                                    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+                                    .join(" ")}
+                            </h4>
+                            <div className="row align-items-center">
+                                <div className="col-12 col-md-3">HP</div>
+                                <div className="col-12 col-md-9">
+                                    <div className="progress">
+                                        <div
+                                            className="progress-bar"
+                                            role="progressBar"
+                                            style={{
+                                                width: `${pokemonDetails.stats.hp}%`,
+                                            }}
+                                            aria-valuenow="25"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                        >
+                                            <small>{pokemonDetails.stats.hp}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-12 col-md-3">Attack</div>
+                                <div className="col-12 col-md-9">
+                                    <div className="progress">
+                                        <div
+                                            className="progress-bar"
+                                            role="progressBar"
+                                            style={{
+                                                width: `${pokemonDetails.stats.attack}%`,
+                                            }}
+                                            aria-valuenow="25"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                        >
+                                            <small>{pokemonDetails.stats.attack}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-12 col-md-3">Defence</div>
+                                <div className="col-12 col-md-9">
+                                    <div className="progress">
+                                        <div
+                                            className="progress-bar"
+                                            role="progressBar"
+                                            style={{
+                                                width: `${pokemonDetails.stats.defence}%`,
+                                            }}
+                                            aria-valuenow="25"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                        >
+                                            <small>{pokemonDetails.stats.defence}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-12 col-md-3">Speed</div>
+                                <div className="col-12 col-md-9">
+                                    <div className="progress">
+                                        <div
+                                            className="progress-bar"
+                                            role="progressBar"
+                                            style={{
+                                                width: `${pokemonDetails.stats.speed}%`,
+                                            }}
+                                            aria-valuenow="25"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                        >
+                                            <small>{pokemonDetails.stats.speed}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-12 col-md-3">Special Attack</div>
+                                <div className="col-12 col-md-9">
+                                    <div className="progress">
+                                        <div
+                                            className="progress-bar"
+                                            role="progressBar"
+                                            style={{
+                                                width: `${pokemonDetails.stats.specialAttack}%`,
+                                            }}
+                                            aria-valuenow="25"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                        >
+                                            <small>{pokemonDetails.stats.specialAttack}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row align-items-center">
+                                <div className="col-12 col-md-3">Special Defence</div>
+                                <div className="col-12 col-md-9">
+                                    <div className="progress">
+                                        <div
+                                            className="progress-bar"
+                                            role="progressBar"
+                                            style={{
+                                                width: `${pokemonDetails.stats.specialDefense}%`,
+                                            }}
+                                            aria-valuenow="25"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                        >
+                                            <small>{pokemonDetails.stats.specialDefense}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mt-1">
+                        <div className="col">
+                            <p className="p-2">{pokemonDetails.description}</p>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="card-body">
+                    <h5 className="card-title text-center">Profile</h5>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h6 className="float-right">Height:</h6>
+                                </div>
+                                <div className="col-md-6">
+                                    <h6 className="float-left">{pokemonDetails.height} ft.</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h6 className="float-right">Weight:</h6>
+                                </div>
+                                <div className="col-md-6">
+                                    <h6 className="float-left">{pokemonDetails.weight} kgs</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h6 className="float-right">Catch Rate:</h6>
+                                </div>
+                                <div className="col-md-6">
+                                    <h6 className="float-left">{pokemonDetails.catchRate}%</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h6 className="float-right">Gender Ratio:</h6>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="progress">
+                                        <div
+                                            className="progress-bar"
+                                            role="progressBar"
+                                            style={{
+                                                width: `${pokemonDetails.genderRatioFemale}%`,
+                                                backgroundColor: "#C2185B",
+                                            }}
+                                            aria-valuenow="15"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                        >
+                                            <small>{pokemonDetails.genderRatioFemale}</small>
+                                        </div>
+                                        <div
+                                            className="progress-bar"
+                                            role="progressBar"
+                                            style={{
+                                                width: `${pokemonDetails.genderRatioMale}%`,
+                                                backgroundColor: "#1976D2",
+                                            }}
+                                            aria-valuenow="30"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                        >
+                                            <small>{pokemonDetails.genderRatioMale}</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h6 className="float-right">Egg Groups:</h6>
+                                </div>
+                                <div className="col-md-6">
+                                    <h6 className="float-left">
+                                        {
+                                            pokemonDetails.eggGroups.join(', ')
+                                        }</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h6 className="float-right">Hatch Steps:</h6>
+                                </div>
+                                <div className="col-md-6">
+                                    <h6 className="float-left">{pokemonDetails.hatchSteps}</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h6 className="float-right">Abilities:</h6>
+                                </div>
+                                <div className="col-md-6">
+                                    <h6 className="float-left">{pokemonDetails.abilities.join(', ')}</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h6 className="float-right">EVs:</h6>
+                                </div>
+                                <div className="col-md-6">
+                                    <h6 className="float-left">{pokemonDetails.evs}</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
